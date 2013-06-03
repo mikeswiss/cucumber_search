e91006d04036546041ba58f26c1405d405208b92
William Albenzi <walbenzi@gmail.com>
2013-06-03 16:40:29 -0700

    Adding pathing for some jenkins testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+export PATH=/usr/local/rvm/gems/ruby-1.9.3-p429/bin:/usr/local/rvm/gems/ruby-1.9.3-p429@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p429/bin:/usr/local/rvm/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 cd $WORKSPACE/ch_4/test_puppies
 HEADLESS=true cucumber --format junit --out $WORKSPACE/report
