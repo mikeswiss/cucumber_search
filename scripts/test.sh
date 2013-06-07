c53c753b49e964bf557b6031d9493c81efc6d6db
Will Albenzi <walbenzi@gmail.com>
2013-06-07 14:18:18 -0700

    adding a line from the windows station

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,3 +3,5 @@
 export PATH=/usr/local/rvm/gems/ruby-1.9.3-p429/bin:/usr/local/rvm/gems/ruby-1.9.3-p429@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p429/bin:/usr/local/rvm/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 cd $WORKSPACE/ch_4/test_puppies
 HEADLESS=true cucumber --format junit --out $WORKSPACE/report
+
+echo "happiness"
