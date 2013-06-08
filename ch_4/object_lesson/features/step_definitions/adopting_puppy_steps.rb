b7a4594266820cbf3c311f4235dca737b17e2681
Will Albenzi <walbenzi@gmail.com>
2013-06-08 11:18:56 -0700

    Adding a homepage object

--- ch_4/object_lesson/features/step_definitions/adopting_puppy_steps.rb
+++ ch_4/object_lesson/features/step_definitions/adopting_puppy_steps.rb
@@ -1,14 +1,15 @@
 Given /^I am on the puppy adoption site$/ do
   @browser.goto "http://puppies.herokuapp.com"
+  @home = HomePage.new(@browser)
 end
 
 When /^I click the first View Details button$/ do 
-  @browser.button(:value => 'View Details', :index => 0).click
+  @home.select_puppy_number 1
   @details = DetailsPage.new(@browser)
 end
 
 When /^I click the second View Details button$/ do
-  @browser.button(:value => 'View Details', :index => 1).click
+  @home.select_puppy_number 2
   @details = DetailsPage.new(@browser)
 end
 
