cbcdbebc2e1518e7600c33998dcbf7ee3ab4a5b3
Will Albenzi <walbenzi@gmail.com>
2013-06-07 20:09:28 -0700

    first attempt at using the PageObject Gem

--- ch_4/object_lesson/features/support/pages/check_out_page.rb
+++ ch_4/object_lesson/features/support/pages/check_out_page.rb
@@ -1,33 +1,11 @@
 class CheckOutPage
+  include PageObject
 
   public
-    def initialize(browser)
-      @browser = browser
-    end
-
-  public
-    def enter_name=(name)
-      @browser.text_field(:id => "order_name").set(name)
-    end
-
-  public
-    def enter_address=(address)
-      @browser.text_field(:id => 'order_address').set(address)
-    end
-
-  public
-    def enter_email=(email)
-      @browser.text_field(:id => 'order_email').set(email)
-    end
-
-  public
-    def enter_pay_with=(pay_with)
-      @browser.select_list(:id => 'order_pay_type').select(pay_with)
-    end
-
-  public
-    def place_order
-      @browser.button(:value => "Place Order").click
-    end
+    text_field(:enter_name, :id => "order_name")
+    text_field(:enter_address, :id => "order_address")
+    text_field(:enter_email, :id => "order_email")
+    select_list(:enter_pay_with, :id => 'order_pay_type')
+    button(:place_order, :value => "Place Order")
 
 end
