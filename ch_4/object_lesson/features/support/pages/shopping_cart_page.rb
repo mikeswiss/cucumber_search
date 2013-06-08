93619f33b4091fceb25af6c46fbf2c5368a4c200
Will Albenzi <walbenzi@gmail.com>
2013-06-07 20:35:51 -0700

    Using the pageobject gem in the shopping cart now too.

--- ch_4/object_lesson/features/support/pages/shopping_cart_page.rb
+++ ch_4/object_lesson/features/support/pages/shopping_cart_page.rb
@@ -1,47 +1,30 @@
 class ShoppingCartPage
+  include PageObject
 
   NAME_COLUMN = 1
   SUBTOTAL_COLUMN = 3
   LINES_PER_PUPPY = 6
 
   public
-    def initialize(browser)
-      @browser = browser
-    end
-
-  private
-    def row_for(line_item)
-      (line_item - 1) * LINES_PER_PUPPY
-    end
+      button(:continue_adopting_puppies, :value => 'Adopt Another Puppy')
+      button(:proceed_to_checkout, :value => 'Complete the Adoption')
+      table(:cart, :index => 0)
+      cell(:cart_total, :class => 'total_cell')
 
   public
     def name_for_line_item(line_item)
-      cart_line_item(line_item)[NAME_COLUMN].text
+      table_value(line_item, NAME_COLUMN)
     end
 
   public
     def subtotal_for_line_item(line_item)
-      cart_line_item(line_item)[SUBTOTAL_COLUMN].text
+      table_value(line_item, SUBTOTAL_COLUMN)
     end
 
   private
-    def cart_line_item(line_item)
-      @browser.table(:index => 0)[row_for(line_item)]
-    end
-
-  public
-    def cart_total
-      @browser.td(:class => 'total_cell').text
-    end
-
-  public
-    def continue_adopting_puppies
-      @browser.button(:value => 'Adopt Another Puppy').click
-    end
-
-  public
-    def proceed_to_checkout
-      @browser.button(:value => 'Complete the Adoption').click
+    def table_value(line_item, column)
+      row = (line_item.to_i - 1) * LINES_PER_PUPPY
+      cart_element[row][column].text
     end
 
 end
