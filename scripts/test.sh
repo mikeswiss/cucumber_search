b3a56aa11e457a1cbf3ee0c50f629b0d777da734
William Albenzi <walbenzi@gmail.com>
2013-06-10 13:49:37 -0700

    moving reports each to a different directory so they are not overwritten.

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,9 +2,9 @@
 
 export PATH=/usr/local/rvm/gems/ruby-1.9.3-p429/bin:/usr/local/rvm/gems/ruby-1.9.3-p429@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p429/bin:/usr/local/rvm/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 cd $WORKSPACE/ch_4/test_puppies
-HEADLESS=true cucumber --format junit --out $WORKSPACE/report
+HEADLESS=true cucumber --format junit --out $WORKSPACE/report/test1
 cd $WORKSPACE/ch_5/test_puppies
-HEADLESS=true BROWSER=firefox cucumber --format junit --out $WORKSPACE/report
-HEADLESS=true BROWSER=chrome cucumber --format junit --out $WORKSPACE/report
+HEADLESS=true BROWSER=firefox cucumber --format junit --out $WORKSPACE/report/test2
+HEADLESS=true BROWSER=chrome cucumber --format junit --out $WORKSPACE/report/test3
 
 echo "happiness"
