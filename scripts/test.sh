53f6529dd03ec8bbad1e69bb1f4c23f9634d908d
William Albenzi <walbenzi@gmail.com>
2013-06-10 13:42:32 -0700

    changing the autotest script to include tests in other chapters

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,5 +3,8 @@
 export PATH=/usr/local/rvm/gems/ruby-1.9.3-p429/bin:/usr/local/rvm/gems/ruby-1.9.3-p429@global/bin:/usr/local/rvm/rubies/ruby-1.9.3-p429/bin:/usr/local/rvm/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin
 cd $WORKSPACE/ch_4/test_puppies
 HEADLESS=true cucumber --format junit --out $WORKSPACE/report
+cd $WORKSPACE/ch_5/test_puppies
+HEADLESS=true BROWSER=firefox cucumber --format junit --out $WORKSPACE/report
+HEADLESS=true BROWSER=chrome cucumber --format junit --out $WORKSPACE/report
 
 echo "happiness"
